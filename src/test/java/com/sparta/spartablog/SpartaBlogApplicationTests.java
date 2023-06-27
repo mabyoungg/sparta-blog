05c3ea0501d4bd5de823e938d2dadf49334fe9d6
mabyoungg <mabyoungg@gmail.com>
2023-06-27 11:22:08 +0900

    init commit

---
+++ src/test/java/com/sparta/spartablog/SpartaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
